2448eef27a8575007b67a6d92f7071bfceb164fb
Tristán Javier Cruz Hernández <124111638+TristanHDZ1@users.noreply.github.com>
2024-11-18 15:20:11 -0600

    Add files via upload

---
+++ Tareas/Loginweb/indexError.php
@@ -0,0 +1,16 @@
+
+<h1>El usuario es incorrecto, por favor intentalo de nuevo</h1>
+<div  >
+    
+        <form method="POST" action="logica/loguear.php">
+
+            <input type="text" name="no_cuenta" placeholder="Numero de Cuenta" />
+            <br />
+            <input type="text" name="clave" placeholder="Contraseña" />
+            <br />
+
+            <button type="submit">Iniciar Sesion</button>
+
+        </form>
+    
+</div>
